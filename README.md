9ae44f991e117991c0f5c45ecd9dd2ecae1709c6
qiuqiu <1813292621@qq.com>
2019-12-05 22:21:46 +0800

    day01 - lambda表达式 测试

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # test_lambda1.8
 java1.8新特性测试demo
 
-一、Lambda表达式
+day01、Lambda表达式
 
 
---
+++ day01-lambda/src/main/java/com/qiu/day01/Day01LambdaApplication.java
@@ -0,0 +1,13 @@
+package com.qiu.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01LambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01LambdaApplication.class, args);
+    }
+
+}
